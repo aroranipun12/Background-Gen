df71202372582ad24014f96da8479e45ff25c537
nipun <aroranipun1@gmail.com>
2020-04-17 23:23:23 +0530

    adding starting files

---
+++ script.js
@@ -0,0 +1,25 @@
+var css = document.querySelector("h3");
+var color1= document.querySelector(".color1");
+var color2= document.querySelector(".color2");
+var body = document.getElementById("grad");
+//console.log(body);
+// console.log(css)
+// console.log(color1)
+// console.log(color2)
+
+function setgrad()
+{
+	body.style.background = 
+	"linear-gradient(to right, "
+	+ color1.value 
+	+ ", "
+	+ color2.value 
+	+	")";
+
+	css.textContent = body.style.background+ ";";
+
+}
+
+color1.addEventListener("input",setgrad);
+
+color2.addEventListener("input",setgrad);
